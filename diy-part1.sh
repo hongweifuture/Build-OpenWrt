e0a248576226118b7fe12fda91fb975d9a7b9f7d
hongweifuture <hongweifuture@163.com>
2021-01-03 22:39:31 +0800

    add self

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,9 +11,9 @@
 #
 
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git self https://github.com/hongweifuture/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/hongweifuture/small' feeds.conf.default
+sed -i '$a src-git hwfuture https://github.com/hongweifuture/openwrt-packages' feeds.conf.default
+sed -i '$a src-git pwdep https://github.com/hongweifuture/pwdep.git' feeds.conf.default
